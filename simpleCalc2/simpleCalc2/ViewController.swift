bd1af4673adcf6780f520032195fc48f31abf4f4
hamzahaly <hzhaly93@gmail.com>
2016-10-26 15:57:30 -0700

    need to figure out how to autolayout

--- simpleCalc2/simpleCalc2/ViewController.swift
+++ simpleCalc2/simpleCalc2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number = Double()
+    var operation = String()
+    var didPress = true
+    var results = Double()
+    var numArray = [Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,9 +29,95 @@ class ViewController: UIViewController {
     @IBOutlet weak var numDisplay: UITextField!
     
     @IBAction func numInput(_ sender: UIButton) {
+        number = number * 10 + Double(sender.titleLabel!.text!)!
+        numDisplay.text = ("\(Int(number))")
     }
 
     @IBAction func operationInput(_ sender: UIButton) {
+        switch operation {
+        case "=":
+            print("equals")
+            print(number)
+            print(results)
+            if (didPress == true) {
+                print("appending")
+                numArray.append(number)
+            }
+            results = number
+            didPress = false
+        case "+":
+            print("adding")
+            results = results + number
+            didPress = false
+        case "-":
+            print("subtracting")
+            results = results - number
+            didPress = false
+        case "*":
+            print("multiplying")
+            results = results * number
+            didPress = false
+        case "/":
+            print("dividing")
+            results = results / number
+            didPress = false
+        case "%":
+            print("modding")
+            var final: Int
+            final = Int(results) % Int(number)
+            results = Double(final)
+            didPress = false
+        case "Fact":
+            print("factorial")
+            var fact = number
+            var operand = number - 1
+            if number == 0 {
+                results = 1
+            } else {
+                for _ in 1...Int(number) - 1 {
+                    fact = fact * operand
+                    print(fact)
+                    operand -= 1
+                    results = fact
+                }
+            }
+            numDisplay.text = ("\(results)")
+        case "Count":
+            print("count")
+            if (didPress == true) {
+                print("appending")
+                numArray.append(number)
+            }
+            didPress = false
+            print(numArray)
+            results = Double(numArray.count)
+        case "Avg":
+            print("avg")
+            if (didPress == true) {
+                numArray.append(number)
+            }
+            didPress = false
+            results = 0.0
+            for num in numArray {
+                results += num
+                print(num)
+                print(results)
+            }
+            print(numArray)
+            results = results / Double(numArray.count)
+        case "Clear":
+            results = number
+            operation = "="
+            numArray.removeAll()
+            numDisplay.text = ("\(results)")
+            didPress = false
+        default:
+            print("error")
+        }
+        number = 0
+        numDisplay.text = ("\(results)")
+        
+        operation = sender.titleLabel!.text!
     }
 }
 
